0e848df37db329e4cd44cc476a357288967cc53b
yizhe-ang <ang.yizhe@u.nus.edu>
2023-07-23 16:31:52 +0800

    use vite 3

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
 import adapter from "@sveltejs/adapter-auto";
 import autoprefixer from "autoprefixer";
-import dsv from "@rollup/plugin-dsv";
-import path from "path";
 import { readFileSync } from "fs";
 import sveltePreprocess from "svelte-preprocess";
-import svg from "vite-plugin-svgstring";
 
-const { version, subdirectory } = JSON.parse(
-  readFileSync("package.json", "utf8")
-);
+const { subdirectory } = JSON.parse(readFileSync("package.json", "utf8"));
 const dev = process.env.NODE_ENV === "development";
 const dir = subdirectory || "";
 const prefix = dir.startsWith("/") ? "" : "/";
@@ -27,25 +22,10 @@ const config = {
     // Prerender everything except pages that are explicitly marked as not prerenderable
     prerender: { default: true },
     files: { lib: "./src" },
-    trailingSlash: "always",
-    vite: {
-      define: { __VERSION__: JSON.stringify(version) },
-      resolve: {
-        alias: {
-          $actions: path.resolve("./src/actions"),
-          $components: path.resolve("./src/components"),
-          $data: path.resolve("./src/data"),
-          $stores: path.resolve("./src/stores"),
-          $styles: path.resolve("./src/styles"),
-          $svg: path.resolve("./src/svg"),
-          $utils: path.resolve("./src/utils")
-        }
-      },
-      plugins: [dsv(), svg()]
-    },
-    paths: {
-      base
-    }
+    trailingSlash: "always"
+  },
+  paths: {
+    base
   }
 };
 
