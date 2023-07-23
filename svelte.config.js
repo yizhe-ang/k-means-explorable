536059eb17b41c86261ab0cf1210b3d3edaa2788
yizhe-ang <ang.yizhe@u.nus.edu>
2023-07-23 15:13:27 +0800

    test

--- svelte.config.js
+++ svelte.config.js
@@ -41,7 +41,10 @@ const config = {
           $utils: path.resolve("./src/utils")
         }
       },
-      plugins: [dsv(), svg()]
+      plugins: [dsv(), svg()],
+      ssr: {
+        noExternal: ["vite"]
+      }
     },
     paths: {
       base
